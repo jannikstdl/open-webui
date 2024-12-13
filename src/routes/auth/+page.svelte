c59c30e0da825072e9697d61389a5a0a459d40da
Jannik Streidl <jannik.stdl@gmail.com>
2024-12-13 07:54:30 +0100

    feat: new auth page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -8,9 +8,10 @@
 	import { toast } from 'svelte-sonner';
 	import { page } from '$app/stores';
 
-	import Hero from '$lib/components/landingpage/Hero.svelte';
-	import FAQ from '$lib/components/landingpage/FAQ.svelte';
-	import Features from '$lib/components/landingpage/Features.svelte';
+	import Tooltip from '$lib/components/common/Tooltip.svelte';
+	import Hero from '$lib/components/fi-ts_landingpage/Hero.svelte';
+	import FAQ from '$lib/components/fi-ts_landingpage/FAQ.svelte';
+	import Features from '$lib/components/fi-ts_landingpage/Features.svelte';
 
 	const i18n = getContext('i18n');
 
@@ -107,9 +108,10 @@
 	</div>
 
 	<div class="overflow-y-auto h-screen">
-		<div class="bg-white dark:bg-gray-950 min-h-screen w-full flex justify-center items-center font-mona">
+		<div
+			class="bg-white dark:bg-gray-950 min-h-screen w-full flex justify-center items-center font-mona"
+		>
 			<div class="w-full sm:max-w-md px-10 flex flex-col text-center">
-
 				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false}
 					<!-- Falls Auth über Header gesetzt oder Auth deaktiviert ist -->
 					<div class="my-auto pb-10 w-full">
@@ -128,10 +130,26 @@
 						</div>
 					</div>
 				{:else}
-					<!-- Auth ist aktiv, also normale Anzeige -->
+					<!-- Auth ist aktiv, normale Anzeige -->
+
+					<!-- Überschrift immer auf der ersten Seite -->
+					{#if !showAdminForm}
+						<div class="mb-6">
+							<div class="font-bold text-left text-3xl text-gray-700 dark:text-gray-300">
+								Anmelden
+								<br />
+								<div
+									class="font-bold text-left text-4xl bg-gradient-to-r from-fits-blue via-gray-600 to-red-600 text-transparent bg-clip-text bg-[length:400%_400%] animate-gradient"
+								>
+									{$WEBUI_NAME}
+								</div>
+							</div>
+						</div>
+					{/if}
+
 					<div class="my-auto pb-10 w-full dark:text-gray-100">
 						{#if !showAdminForm}
-							<!-- Initial: nur Oauth Button + Administrativer Login Button -->
+							<!-- Startseite mit Oauth -->
 							{#if Object.keys($config?.oauth?.providers ?? {}).length > 0}
 								<!-- Oauth Buttons -->
 								<div class="flex flex-col space-y-2">
@@ -142,7 +160,11 @@
 												window.location.href = `${WEBUI_BASE_URL}/oauth/google/login`;
 											}}
 										>
-											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="size-6 mr-3">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 48 48"
+												class="size-6 mr-3"
+											>
 												<path
 													fill="#EA4335"
 													d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
@@ -168,7 +190,11 @@
 												window.location.href = `${WEBUI_BASE_URL}/oauth/microsoft/login`;
 											}}
 										>
-											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21" class="size-6 mr-3">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 21 21"
+												class="size-6 mr-3"
+											>
 												<rect x="1" y="1" width="9" height="9" fill="#f25022" /><rect
 													x="1"
 													y="11"
@@ -183,50 +209,91 @@
 													fill="#ffb900"
 												/>
 											</svg>
-											<span>{$i18n.t('Continue with {{provider}}', { provider: 'Microsoft' })}</span>
+											<span>{$i18n.t('Continue with {{provider}}', { provider: 'Microsoft' })}</span
+											>
 										</button>
 									{/if}
 
 									{#if $config?.oauth?.providers?.oidc}
-										<button
-											class="flex items-center px-6 border-2 dark:border-gray-800 duration-300 dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 w-full rounded-2xl dark:text-white text-sm py-3 transition justify-center"
-											on:click={() => {
-												window.location.href = `${WEBUI_BASE_URL}/oauth/oidc/login`;
-											}}
+										<Tooltip
+											content="Hier mit IZ-Nummer (oder E-Mail) und aktuellem OfficeLAN Passwort einloggen."
 										>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												class="size-6 mr-3"
+											<button
+												class="flex items-center px-6 border-2 dark:border-gray-800 duration-300 dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 w-full rounded-2xl dark:text-white text-sm py-3 transition justify-center"
+												on:click={() => {
+													window.location.href = `${WEBUI_BASE_URL}/oauth/oidc/login`;
+												}}
 											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"
-												/>
-											</svg>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke-width="1.5"
+													stroke="currentColor"
+													class="size-6 mr-3"
+												>
+													<path
+														stroke-linecap="round"
+														stroke-linejoin="round"
+														d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"
+													/>
+												</svg>
 
-											<span
-												>{$i18n.t('Continue with {{provider}}', {
-													provider: $config?.oauth?.providers?.oidc ?? 'SSO'
-												})}</span
-											>
-										</button>
+												<span
+													>{$i18n.t('Continue with {{provider}}', {
+														provider: $config?.oauth?.providers?.oidc ?? 'SSO'
+													})}</span
+												>
+											</button>
+										</Tooltip>
 									{/if}
+								</div>
+							{/if}
+
+							<!-- "oder" Trennstrich -->
+							{#if Object.keys($config?.oauth?.providers ?? {}).length > 0}
+								<div class="relative w-full">
+									<hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 mx-auto" />
+									<div
+										class="absolute px-3 font-medium text-gray-900 bg-white left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 dark:text-white dark:bg-gray-950"
+									>
+										{$i18n.t('or')}
+									</div>
 								</div>
 							{/if}
 
 							<!-- Administrativer Login Button -->
 							<button
-								class="mt-6 text-sm rounded-2xl border border-gray-300 dark:border-gray-700 py-2 px-4 text-gray-600 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+								class="text-sm rounded-2xl border border-gray-300 dark:border-gray-700 py-2 px-4 text-gray-600 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
 								on:click={() => (showAdminForm = true)}
 							>
 								Administrativer Login
 							</button>
 						{:else}
+							<!-- Admin Login -->
+							<!-- Back Button -->
+							<div class="flex items-center justify-start mb-4">
+								<button
+									class="rounded-full border border-gray-300 dark:border-gray-700 p-2 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+									on:click={() => (showAdminForm = false)}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="w-5 h-5 text-gray-600 dark:text-gray-300"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M15 19l-7-7 7-7"
+										/>
+									</svg>
+								</button>
+							</div>
+
 							<!-- Administrativer Login Formular -->
 							<form
 								class="flex flex-col justify-center"
@@ -235,16 +302,6 @@
 								}}
 							>
 								<div class="mb-1">
-									<div class="font-bold text-left text-3xl text-gray-700 dark:text-gray-300">
-										{$i18n.t('Sign in')}
-										<br />
-										<div
-											class="font-bold text-left text-4xl bg-gradient-to-r from-fits-blue via-gray-600 to-red-600 text-transparent bg-clip-text bg-[length:400%_400%] animate-gradient"
-										>
-											{$WEBUI_NAME}
-										</div>
-									</div>
-
 									<div class="flex flex-col mt-4">
 										<div class="mb-2">
 											<div class="text-sm font-medium text-left mb-1">{$i18n.t('Email')}</div>
@@ -306,13 +363,31 @@
 
 <style>
 	.font-mona {
-		font-family: 'Mona Sans', -apple-system, 'Arimo', ui-sans-serif, system-ui, 'Segoe UI', Roboto,
-			Ubuntu, Cantarell, 'Noto Sans', sans-serif, 'Helvetica Neue', Arial, 'Apple Color Emoji',
-			'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+		font-family:
+			'Mona Sans',
+			-apple-system,
+			'Arimo',
+			ui-sans-serif,
+			system-ui,
+			'Segoe UI',
+			Roboto,
+			Ubuntu,
+			Cantarell,
+			'Noto Sans',
+			sans-serif,
+			'Helvetica Neue',
+			Arial,
+			'Apple Color Emoji',
+			'Segoe UI Emoji',
+			'Segoe UI Symbol',
+			'Noto Color Emoji';
 	}
 
 	.fade-in-up {
-		transition: opacity 0.2s ease-out, transform 0.2s ease-out, max-height 0.2s ease-out;
+		transition:
+			opacity 0.2s ease-out,
+			transform 0.2s ease-out,
+			max-height 0.2s ease-out;
 		opacity: 0;
 		transform: translateY(15px);
 		max-height: 0;
