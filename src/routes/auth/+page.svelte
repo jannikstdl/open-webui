b7b1dc1224cdf70b07c1c4203424a5bd08e88a2e
Streidl, Jannik <jannik.streidl@f-i-ts.de>
2024-12-13 11:18:14 +0000

    Update +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -184,35 +184,51 @@
 												on:click={handleOAuthClick}
 												disabled={!$config?.oauth?.providers?.oidc}
 											>
-												<!-- Schlüssel-SVG -->
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													stroke-width="1.5"
-													stroke="currentColor"
-													class="w-6 h-6 mr-3 {!$config?.oauth?.providers?.oidc
-														? 'text-red-500 dark:text-red-400'
-														: ''}"
-												>
-													<path
-														stroke-linecap="round"
-														stroke-linejoin="round"
-														d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 
-													5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1
-													.43-1.563A6 6 0 1 1 21.75 8.25Z"
-													/>
-												</svg>
-
 												{#if $config?.oauth?.providers?.oidc}
-													<span class="transition-opacity duration-200 group-hover:opacity-0">
+													<!-- Normalzustand (mit Schlüssel) -->
+													<span
+														class="transition-opacity duration-200 group-hover:opacity-0 flex items-center"
+													>
+														<!-- Schlüssel SVG -->
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															stroke-width="1.5"
+															stroke="currentColor"
+															class="w-6 h-6 mr-3"
+														>
+															<path
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 
+					5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1
+					.43-1.563A6 6 0 1 1 21.75 8.25Z"
+															/>
+														</svg>
 														{$i18n.t('Continue with {{provider}}', {
 															provider: $config?.oauth?.providers?.oidc ?? 'SSO'
 														})}
 													</span>
+
+													<!-- Hoverzustand (mit Pfeil) -->
 													<span
 														class="absolute inset-0 flex items-center justify-center text-xs text-gray-600 dark:text-gray-400 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
 													>
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															stroke-width="1.5"
+															stroke="currentColor"
+															class="w-4 h-4 mr-1"
+														>
+															<path
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																d="M8.25 4.5l7.5 7.5-7.5 7.5"
+															/>
+														</svg>
 														IZ-Nr. & OfficeLAN-Passwort
 													</span>
 												{:else}
