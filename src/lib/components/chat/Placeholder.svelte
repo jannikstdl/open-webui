ce9a9eb16b0f92348921bdbcef6d0b4c25fa9c59
Jannik Streidl <jannik.stdl@gmail.com>
2025-02-11 16:27:11 +0100

    wording

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -91,7 +91,7 @@
 <div class="m-auto w-full max-w-6xl px-2 @2xl:px-20 translate-y-6 py-24 text-center">
 	{#if $temporaryChatEnabled}
 		<Tooltip
-			content="This chat won't appear in history and your messages will not be saved."
+			content="Dieser Chat wird weder in der Historie angezeigt noch gespeichert."
 			className="w-full flex justify-center mb-0.5"
 			placement="top"
 		>
